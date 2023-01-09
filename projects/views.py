2da9cfee8d9f2aa1b84b349fc98febf1bcc8b352
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2023-01-09 16:47:15 +0800

    Added templates and urls for projects app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+projects = [
+    {
+        "id": 1,
+        "title" : "Project 1",
+        "description": "lorem ipsum test test 1"
+    },
+    {
+        "id": 2,
+        "title" : "Project 2",
+        "description": "lorem ipsum test test 2"
+    },
+    {
+        "id": 3,
+        "title" : "Project 3",
+        "description": "lorem ipsum test test 3"
+    },
+]
+
+def index(request):
+    context = {"projects":projects}
+    return render(request, 'projects/index.html', context)
