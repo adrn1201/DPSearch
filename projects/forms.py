ed68784647b389e63f81de109a6fe1d693550b87
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2023-01-10 13:46:33 +0800

    added project template

--- projects/forms.py
+++ projects/forms.py
@@ -13,3 +13,10 @@ class ProjectForm(ModelForm):
             'source_link'
         ]
     
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "input"})
+    
