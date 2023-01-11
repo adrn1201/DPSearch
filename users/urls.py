c920d6da19c5e468b83fc71e22c2024b6cf2a693
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2023-01-11 20:20:51 +0800

    added user authentication v1

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
+    
     path('', views.index, name="profiles"),
     path('profile/<slug:slug>', views.show_profile, name="user-profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,62 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, authenticate, logout
+
+from django.contrib.auth.models import User
 from .models import Profile
+
+from .forms import CustomUserCreationForm
+
+
+def login_user(request):
+    
+    if request.user.is_authenticated:
+        return redirect('profiles')
+    
+    if request.method == "POST":
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            pass
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            pass
+        
+    return render(request, "users/login_register.html")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def register_user(request):
+    page = "register"
+    form = CustomUserCreationForm()
+    
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            
+            login(request, user)
+            
+            return redirect('profiles')
+        else:
+            pass
+    
+    context = {"page": page, "form" : form}
+    return render(request, "users/login_register.html", context)
+    
 
 
 def index(request):
