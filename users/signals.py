8e09b286844e3344c20f538a0f44c729ef0d951a
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2023-01-12 11:17:46 +0800

    added profile signal creation

--- users/signals.py
+++ users/signals.py
@@ -7,3 +7,43 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 
+import string 
+from django.utils.text import slugify 
+import random 
+
+
+def random_string_generator(size = 10, chars = string.ascii_lowercase + string.digits): 
+    return ''.join(random.choice(chars) for _ in range(size)) 
+   
+
+def unique_slug_generator(instance, new_slug = None): 
+    if new_slug is not None: 
+        slug = new_slug 
+    else: 
+        slug = slugify(instance.name) 
+    class_instance = instance.__class__ 
+    qs_exists = class_instance.objects.filter(slug = slug).exists() 
+      
+    if qs_exists: 
+        new_slug = "{slug}-{randstr}".format( 
+            slug = slug, randstr = random_string_generator(size = 4)) 
+              
+        return unique_slug_generator(instance, new_slug = new_slug) 
+    return slug 
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user = user,
+            username = user.username,
+            email = user.email,
+            name = user.first_name + " " + user.last_name  
+        )
+        
+        if not profile.slug:
+            profile.slug = unique_slug_generator(profile) 
+            profile.save()
+
